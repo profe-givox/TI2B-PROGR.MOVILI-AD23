0c177ca8d9ee74660b968f587cacf61a55ce44b8
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-08-31 12:59:59 -0600

    Se agregaron las imagenes de las caras de los dados SE agrego el componente Image y Button Sobre el proyecto dice roller

--- DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -3,12 +3,19 @@ package net.ivanvega.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.ivanvega.diceroller.ui.theme.DiceRollerTheme
 
@@ -22,7 +29,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    DiceRollerApp()
                 }
             }
         }
@@ -32,15 +40,34 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+
+        )
+
+}
+
+val l = @Composable(){
 
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = stringResource(
+            R.string.image_cara_1
+        ) )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.btn_roller))
+        }
+    }
 
 }
 
 
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -55,4 +82,4 @@ fun GreetingPreview() {
     DiceRollerTheme {
         Greeting("Android")
     }
-}
+}*/
